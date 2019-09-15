49e649b36f7821691cbb147db1d67dbfc913cfff
Clark Chan <chan239@purdue.edu>
2019-09-15 19:41:42 -0400

    put lines to separate elements

--- linux_sheets.py
+++ linux_sheets.py
@@ -3,5 +3,9 @@ import sys
 class linux_sheets():
     file_name = sys.argv[1]
     with open(file_name, 'r') as file:
-        contents = [line.split() for line in file]
-        print contents
+        contents = csv.reader(file, delimiter=',')
+        for row in contents:
+            print "|",
+            for index in range(len(row)):
+                print row[index]+" |",
+            print ""

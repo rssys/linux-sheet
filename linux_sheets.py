902aa2b530f4f1ed0b91e885e18439743731d17f
Clark Chan <chan239@purdue.edu>
2019-09-15 19:32:06 -0400

    made the program print rows

--- linux_sheets.py
+++ linux_sheets.py
@@ -1,1 +1,7 @@
-
+import csv
+import sys
+class linux_sheets():
+    file_name = sys.argv[1]
+    with open(file_name, 'r') as file:
+        contents = [line.split() for line in file]
+        print contents

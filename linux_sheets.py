d54fcec5295e8014170b8776a20706cf665c535f
Clark Chan <chan239@purdue.edu>
2019-10-03 17:13:59 -0400

    made it so the program will fill in spaces that aren't on screen when user scrolls

--- linux_sheets.py
+++ linux_sheets.py
@@ -16,22 +16,31 @@ def createGrid(stdscr, data, max_row_len, current_row_idx, current_col_idx):
     # cell width and height let us do formatted printing and navigate through each cell
     cell_h = 2
     cell_w = 12
+
     # offsets for when user scrolls down
     h_offset = 0
     w_offset = 0
+
     # gap at top of screen for the bar
     top_bar_h = 1
+
+    # height of the grid window
+    grid_h = h - top_bar_h
+
     # constant to make sure we jump to the start of the word and not the edge of a cell
     dist_from_wall = 1
 
+    # set offsets
     if current_row_idx * cell_h >= h:
         h_offset = current_row_idx * cell_h - h
-        if h_offset % 2 == 1:
-            h_offset += 1
+        h_offset += h_offset % cell_h
     if current_col_idx * cell_w > w:
         w_offset = current_col_idx * cell_w - w
+        w_offset += w_offset % cell_w
+
     # create the grid
     grid = curses.newpad(h + h_offset, w + w_offset)
+
     # loop through array
     for row_idx, row in enumerate(data):
         y = (row_idx * cell_h)
@@ -40,8 +49,9 @@ def createGrid(stdscr, data, max_row_len, current_row_idx, current_col_idx):
             # right_line_x = x+10
             # grid.vline(0,right_line_x,'|',h+h_offset)
             if col_idx < len(data[row_idx]):
-                if(x+1 < w):
+                if y < h + h_offset and x+1 < w+w_offset:
                     grid.addstr(y,x+1, data[row_idx][col_idx])
+
     # draw the horizontal lines
     for h_line in range((h+h_offset)//cell_h):
         y = (h_line * cell_h) + 1
@@ -50,23 +60,24 @@ def createGrid(stdscr, data, max_row_len, current_row_idx, current_col_idx):
     for v_line in range(1,(w+w_offset)//cell_w):
         x = (v_line * cell_w)
         grid.vline(0,x,'|',h+h_offset)
+
     # print("current",current_row_idx * cell_h,"height = ",h+h_offset)
     # refresh pad depending on where user is and move cursor
-    if current_row_idx * cell_h >= h:
-        if current_col_idx * cell_w+dist_from_wall >= w:
+    if current_row_idx * cell_h >= grid_h:
+        if current_col_idx * cell_w + dist_from_wall >= w:
             grid.move((current_row_idx * cell_h) - cell_h, dist_from_wall+(current_col_idx * cell_w) - cell_w)
-            grid.refresh(h_offset,w_offset,top_bar_h,0,h-top_bar_h,w)
+            grid.refresh(h_offset,w_offset,top_bar_h,0,grid_h,w)
         else:
             # print(h_offset)
             grid.move((current_row_idx * cell_h), dist_from_wall+(current_col_idx * cell_w))
-            grid.refresh(h_offset,0,top_bar_h,0,h-top_bar_h,w)
+            grid.refresh(h_offset,0,top_bar_h,0,grid_h,w)
     else:
-        if current_col_idx * cell_w+dist_from_wall >= w:
+        if current_col_idx * cell_w + dist_from_wall >= w:
             grid.move((current_row_idx * cell_h), dist_from_wall+(current_col_idx * cell_w) - cell_w)
-            grid.refresh(0,w_offset,top_bar_h,0,h-top_bar_h,w)
+            grid.refresh(0,w_offset,top_bar_h,0,grid_h,w)
         else:
             grid.move((current_row_idx * cell_h), dist_from_wall+(current_col_idx * cell_w))
-            grid.refresh(0,0,top_bar_h,0,h-top_bar_h,w)
+            grid.refresh(0,0,top_bar_h,0,grid_h,w)
 
 
 def main(stdscr):
